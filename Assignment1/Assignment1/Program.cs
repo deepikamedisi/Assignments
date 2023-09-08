c8397f70e4e3d915870832d55f7ead11d5687b09
Deepika Medisi <deepikam@infinite.com>
2023-09-08 12:04:59 +0530

    1st assignment

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int fnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int snum = Convert.ToInt32(Console.ReadLine());
+            if (fnum == snum)
+
+                Console.WriteLine($"{fnum} and {snum} are equal");
+
+            else
+
+                Console.WriteLine($"{fnum} and {snum} are not equal");
+
+            Program.PosOrNeg();
+            Program.operations();
+
+            Console.Read();
+
+
+        }
+        public static void PosOrNeg()
+        {
+            Console.WriteLine("enter the value");
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            if (i >=0)
+            {
+                Console.WriteLine($"{i} is a positive");
+                 Console.ReadLine();
+            }
+           
+            else
+            {
+                Console.WriteLine($"{i} is a negative");
+
+            }
+            Console.ReadLine();
+        }
+
+        public static void operations()
+        {
+            Console.WriteLine("Enter the first number");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int  sum = j + k;
+            Console.WriteLine("{0}", sum);
+            int sub = j - k;
+            Console.WriteLine("{0}" ,sub);
+            int mul = j * k;
+            Console.WriteLine("{0}", mul);
+            int div = j / k;
+            Console.WriteLine("{0}", div);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
---
+++ assignment2/assignment2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    class Array_prog
+    {
+        public static void Avgofarray()
+        {
+            Console.WriteLine("enter the size of an array");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("enter the elements of an array");
+            for (int i=0; i<size;i++)
+            {
+                Console.Write($"Enter element {i}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+             
+            }
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine($"average of an array {arr.Average()}");
+            Console.WriteLine($"The maximum value {arr.Max()}");
+            Console.WriteLine($"The minimum value {arr.Min()}");
+
+        }
+
+        public static void marks()
+        {
+            int[] ar = new int[10];
+            Console.WriteLine("enter the elements of an array");
+            for (int j = 0; j < ar.Length; j++)
+            {
+               Console.Write($"Enter element {j}: ");
+               ar[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            int total = 0;
+            for (int j = 0; j < ar.Length; j++)
+            {
+                total = total + ar[j];
+            }
+
+            Console.WriteLine($"The total marks is {total}");
+            Console.WriteLine($"average of the total marks is {ar.Average()}");
+            Console.WriteLine($"minimum mark {ar.Max()}");
+            Console.WriteLine($"maximum mark {ar.Min()}");
+
+            Array.Sort(ar);
+            Console.Write("Ascending order of an array is : ");
+            for(int j=0;j<ar.Length;j++)
+            {
+                
+                Console.WriteLine($" {ar[j]}");
+            }
+            Array.Reverse(ar);
+            Console.WriteLine("Descending order of an array is :");
+            for(int j=0;j<ar.Length;j++)
+            {
+                Console.WriteLine($" {ar[j]}");
+            }
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array_prog.Avgofarray();
+            Array_prog.marks();
+        }
+
+    }
+}
