76809377e8b7db4070cfad13daec3eb2a877de27
Deepika Medisi <deepikam@infinite.com>
2023-09-10 19:07:30 +0530

    assessment3

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class stringprog
+    {
+        public static void strlen()
+        {
+            Console.WriteLine("Enter the string");
+            string s = Console.ReadLine();
+            int len = s.Length;
+            Console.WriteLine("The string length is: {0} ", len);
+        }
+        public static void strreverse()
+        {
+            Console.WriteLine("Enter the string");
+            string s = Console.ReadLine();
+            char[] s1 = s.ToCharArray();
+            Array.Reverse(s1);
+            string reversedword = new string(s1);
+            Console.WriteLine($"The reversed string is  {reversedword}");
+
+        }
+        public static void strcompare()
+        {
+            Console.WriteLine("Enter the first string ");
+            string s1 = Console.ReadLine();
+            Console.WriteLine("Enter the second string ");
+            string s2 = Console.ReadLine();
+            if(string.Equals (s1,s2))
+            {
+                Console.WriteLine("Both strings are same");
+            }
+            else
+            {
+                Console.WriteLine("Both strings are not same");
+            }
+        }
+       
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            stringprog.strlen();
+            stringprog.strreverse();
+            stringprog.strcompare();
+            Console.Read();
+        }
+    }
+}
