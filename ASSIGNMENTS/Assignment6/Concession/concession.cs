e0ec9c542152d1f064186b7e6dd2d958e1f66305
Deepika Medisi <deepikam@infinite.com>
2023-09-23 19:33:39 +0530

    my git

---
+++ ASSIGNMENTS/Assignment6/Concession/concession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concession
+{
+    public class concession
+    {
+        public const int TotalFare= 500;
+        public void CalculateConcession(string Name,int age)
+        {
+              if(age<=5)
+              {
+                Console.WriteLine($"Name: {Name}  Age:{age}" );
+                Console.WriteLine("Little Champs -Free Ticket");
+
+              }
+              else if(age>60)
+              {
+                double ConcessionAmount = 0.3 * TotalFare;
+                double deductedAmount = TotalFare - ConcessionAmount;
+                Console.WriteLine($"Name: {Name}  Age:{age}");
+                Console.WriteLine($"Senior Citizen ,Fare: {deductedAmount}");
+              }
+              else
+              {
+                Console.WriteLine($"Name: {Name}  Age:{age}");
+                Console.WriteLine($"Ticket Booked ,Fare:{TotalFare}");
+
+              }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ASSIGNMENTS/Assignment6/Concessionmain/Concessionmain/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concessionmain
+{
+    class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter the Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Concession.concession cc = new Concession.concession();
+            cc.CalculateConcession(Name,age);
+        }
+        
+    }
+}
