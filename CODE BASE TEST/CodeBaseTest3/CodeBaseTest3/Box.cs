1ef664d24f1d58955a66835b8c4ccc45d3a1d0f9
Deepika Medisi <deepikam@infinite.com>
2023-09-25 13:19:05 +0530

    my git

---
+++ CODE BASE TEST/CodeBaseTest3/CodeBaseTest3/Box.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Box
+{
+    public double Length { get; set; }
+    public double Breadth { get; set; }
+
+    public Box(double Length, double Breadth)
+    {
+       this. Length = Length;
+       this.Breadth = Breadth;
+    }
+
+    public static Box Add(Box b1, Box b2)
+    {
+        double sumLength = b1.Length + b2.Length;
+        double sumBreadth = b1.Breadth + b2.Breadth;
+        return new Box(sumLength, sumBreadth);
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Length: {Length}, Breadth: {Breadth}");
+    }
+}
+
+class BoxTest
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the length of Box 1: ");
+        double Length1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the breadth of Box 1: ");
+        double Breadth1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the length of Box 2: ");
+        double Length2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the breadth of Box 2: ");
+        double Breadth2 = Convert.ToDouble(Console.ReadLine());
+
+        Box box1 = new Box(Length1, Breadth1);
+        Box box2 = new Box(Length2, Breadth2);
+
+        Box box3 = Box.Add(box1, box2);
+
+        Console.WriteLine("Box 3 : Sum of Box 1 and Box 2 ");
+        box3.DisplayDetails();
+        Console.ReadLine();
+    }
+}
---
+++ CODE BASE TEST/CodeBaseTest3/CodeBaseTest3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBaseTest3
+{
+    class Cricket
+    {
+        public void Pointscalculation(int no_of_matches)
+        {
+            int[] scores = new int[no_of_matches];
+            double sum = 0;
+            for(int i=0;i<no_of_matches;i++)
+            {
+                Console.WriteLine($"Enter score for match{i + 1}");
+                int score = int.Parse(Console.ReadLine());
+                sum += score;
+            }
+            double average = sum / no_of_matches;
+            Console.WriteLine($"The sum of the scores {sum}");
+            Console.WriteLine($"The average score {average}");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the no.of matches");
+            int no_of_matches = Convert.ToInt32(Console.ReadLine());
+            Cricket cricketmatch = new Cricket();
+            cricketmatch.Pointscalculation(no_of_matches);
+            
+            Console.ReadLine();
+        }
+    }
+}
