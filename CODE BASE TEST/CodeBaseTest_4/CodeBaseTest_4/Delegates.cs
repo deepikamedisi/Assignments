673d3e50ead0e337a9bd0173988439be44fb4825
Deepika Medisi <deepikam@infinite.com>
2023-10-03 18:03:03 +0530

    my git

---
+++ CODE BASE TEST/CodeBaseTest_4/CodeBaseTest_4/Delegates.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    delegate int CalculatorDelegate(int number1, int number2);
+
+    class Calculator
+    {
+        public static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        public static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Calculator App!");
+            CalculatorDelegate calculatorDelegate = null;
+
+            while (true)
+            {
+                Console.WriteLine("Select an operation:");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Quit");
+
+                if (!int.TryParse(Console.ReadLine(), out int choice) || choice < 1 || choice > 4)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid option.");
+                    continue;
+                }
+
+                if (choice == 4)
+                {
+                    Console.WriteLine("Quitting the calculator");
+                    break;
+                }
+
+                Console.Write("Enter the first number: ");
+                if (!int.TryParse(Console.ReadLine(), out int num1))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+
+                Console.Write("Enter the second number: ");
+                if (!int.TryParse(Console.ReadLine(), out int num2))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        calculatorDelegate = Calculator.Add;
+                        break;
+                    case 2:
+                        calculatorDelegate = Calculator.Subtract;
+                        break;
+                    case 3:
+                        calculatorDelegate = Calculator.Multiply;
+                        break;
+                }
+
+                if (calculatorDelegate != null)
+                {
+                    int result = calculatorDelegate(num1, num2);
+                    Console.WriteLine($"Result: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation. Please select a valid operation.");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ CODE BASE TEST/CodeBaseTest_4/CodeBaseTest_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class file
+{
+    static void Main()
+    {
+        string filePath = "D:\\training programs\\CodeBaseTest_4\\samples.txt";
+        string textToAppend = "This text will be appended to the file.";
+
+       
+        if (!File.Exists(filePath))
+        {
+            using (StreamWriter writer = File.CreateText(filePath))
+            {
+                writer.WriteLine(textToAppend);
+            }
+            Console.WriteLine("File created and text has been added.");
+        }
+        else
+        {
+            
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(textToAppend);
+            }
+            Console.WriteLine("Text has been appended to the file.");
+        }
+        Console.ReadLine();
+    }
+}
