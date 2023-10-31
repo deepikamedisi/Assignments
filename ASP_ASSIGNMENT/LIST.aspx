f2d65c0e638e76747ed8fb5da33226c8bfdad2dc
Deepika Medisi <deepikam@infinite.com>
2023-10-31 16:36:08 +0530

    Asp_assignment

---
+++ ASP_ASSIGNMENT/LIST.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LIST.aspx.cs" Inherits="ASPASSIGNMENT.LIST" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+   <form id="form1" runat="server">
+        <div>
+            <h1>Select a Bike Model:</h1>
+            <asp:DropDownList ID="ddlItems" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlItems_SelectedIndexChanged">
+                <asp:ListItem Text="Select a Bike" Value="0" />
+                <asp:ListItem Text="KTM" Value="1"  />
+                <asp:ListItem Text="M_15" Value="2"  />
+                <asp:ListItem Text="ROYAL_ENFIELD" Value="3"  />
+                <asp:ListItem Text="PULSAR" Value="4"  />
+
+            </asp:DropDownList>
+            <br /><br />
+
+            <asp:Image ID="imgItem"  runat="server" ImageUrl="Images/default.jfif" Width="200" Height="200" />
+
+            <br /><br />
+
+            <asp:Button ID="btnShowCost" runat="server" Text="Show Cost" OnClick="btnShowCost_Click" />
+            <br /><br />
+
+            <asp:Label ID="lblCost" runat="server" Text="" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP_ASSIGNMENT/LIST.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPASSIGNMENT
+{
+    public partial class LIST : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Set the default image when the page loads
+                imgItem.ImageUrl = "Images/default.jfif";
+            }
+        }
+
+        protected void btnShowCost_Click(object sender, EventArgs e)
+        {
+            // Replace this with your logic to calculate and display the cost
+            // Here, we're just displaying a placeholder message
+            string selectedItem = ddlItems.SelectedValue;
+            string cost = string.Empty;
+            if (selectedItem == "1")
+            {
+                
+                cost = "100000";
+
+            }
+
+            else if (selectedItem == "2")
+            {
+               
+                cost = "150000";
+            }
+
+            else if (selectedItem == "3")
+            {
+               
+                cost = "200000";
+            }
+            else if (selectedItem == "4")
+            {
+               
+                cost = "80000";
+            }
+
+            string Item = ddlItems.SelectedItem.Text;
+
+            lblCost.Text = $"Cost of {Item}: {cost}";
+
+        }
+
+        protected  void ddlItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // When an item is selected, update the image accordingly
+            string selectedItem = ddlItems.SelectedValue;
+            string cost = string.Empty;
+            if (selectedItem == "1")
+            {
+                imgItem.ImageUrl = $"Images/KTM.jfif";
+               
+                
+            }
+
+            else if (selectedItem == "2")
+            {
+                imgItem.ImageUrl = $"Images/MT 15.jfif";
+                
+            }
+
+            else if (selectedItem == "3")
+            {
+                imgItem.ImageUrl = $"Images/ROYAL ENFIELD.jfif";
+               
+            }
+            else if (selectedItem == "4")
+            {
+                imgItem.ImageUrl = $"Images/PULSAR.jfif";
+                
+            }
+            else
+            {
+
+                imgItem.ImageUrl = $"Images/{"Images/default.jfif"}";
+            }
+        }
+    }
+        
+    
+}
