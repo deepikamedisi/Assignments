8b2328c346994c16da79715bc1513736c6b92126
Deepika Medisi <deepikam@infinite.com>
2023-11-11 15:31:41 +0530

    my git

---
+++ CODE BASE TEST/MVC_CODEBASETEST/PROJECT_2/PROJECT_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PROJECT_2.MvcApplication" Language="C#" %>
---
+++ CODE BASE TEST/MVC_CODEBASETEST/PROJECT_2/PROJECT_2/Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROJECT_2.Models
+{
+    public class Movies
+    { 
+            public int Mid { get; set; }
+            public string MovieName { get; set; }
+            public DateTime DateofRelease { get; set; }
+        
+    }
+}
